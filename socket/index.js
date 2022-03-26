35d471fd578bfee65fa1b39a5153d14621d66d6f
natnael-OQP <makiya112545@gmail.com>
2022-03-26 23:12:08 +0300

    addnewuser

--- socket/index.js
+++ socket/index.js
@@ -6,9 +6,31 @@ const io = new Server(5000, {
   },
 });
 
+let users = [];
+
+const addNewUser = (username, socketId) => {
+  !users.some((user) => user.username === username) &&
+    users.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (username) => {
+  return (users = users.filter((user) => user.username === username));
+};
+
 io.on("connection", (socket) => {
-  console.log("someone has connected");
+  // ad new user
+  socket.on("addNewUser", (username) => {
+    addNewUser(username, socket.id);
+    io.emit("getUser", users);
+  });
+
+  // remove user
   socket.on("disconnect", () => {
-    console.log("someone has disconnected");
+    removeUser(socket.id);
+    io.emit("getUser", users);
   });
 });
