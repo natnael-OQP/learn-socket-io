f728d30f1a1f448d26f6c1a4d65eb31ef74e6e21
natnael-OQP <makiya112545@gmail.com>
2022-03-26 17:35:40 +0300

    homepage created

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+import Card from "./components/card/Card";
+import Navbar from "./components/navbar/Navbar";
+import { posts } from "./data";
+import { io } from "socket.io-client";
+
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    // setSocket(io("http://localhost:5000"));
+  }, []);
+
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <Navbar />
+          {posts.map((post) => (
+            <Card key={post.id} post={post} user={user} />
+          ))}
+          <span className="username">{user}</span>
+        </>
+      ) : (
+        <div className="login">
+          <h2>Lama App</h2>
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={() => setUser(username)}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
