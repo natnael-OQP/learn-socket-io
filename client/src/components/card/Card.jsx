6c3d348a84b521aa1078caec26335f212bdba9da
natnael-OQP <makiya112545@gmail.com>
2022-03-26 23:10:46 +0300

    pass user and socket

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -6,16 +6,12 @@ import Share from "../../img/share.svg";
 import Info from "../../img/info.svg";
 import { useState } from "react";
 
-const Card = ({ post, socket, user }) => {
+const Card = ({ post, user, socket }) => {
   const [liked, setLiked] = useState(false);
 
   const handleNotification = (type) => {
     type === 1 && setLiked(true);
-    socket.emit("sendNotification", {
-      senderName: user,
-      receiverName: post.username,
-      type,
-    });
+    socket.emit("notification");
   };
 
   return (
